3859aad3deeecd7b2e6bfdc2b916066616cf1577
Kelvin Fok <punkd1@gmail.com>
2019-03-10 22:43:43 +0800

    lesson 14

--- TinderFirestore/Controllers/RegistrationViewController.swift
+++ TinderFirestore/Controllers/RegistrationViewController.swift
@@ -43,6 +43,7 @@ class RegistrationViewController: UIViewController {
     let fullNameTextField: CustomTextField = {
         let tf = CustomTextField(padding: 16, height: 48)
         tf.placeholder = "Enter full name"
+        tf.autocorrectionType = .no
         tf.backgroundColor = .white
         return tf
     }()
@@ -51,6 +52,7 @@ class RegistrationViewController: UIViewController {
         let tf = CustomTextField(padding: 16, height: 48)
         tf.placeholder = "Enter email"
         tf.backgroundColor = .white
+        tf.keyboardType = .emailAddress
         return tf
     }()
     
@@ -58,6 +60,7 @@ class RegistrationViewController: UIViewController {
         let tf = CustomTextField(padding: 16, height: 48)
         tf.placeholder = "Enter password"
         tf.isSecureTextEntry = true
+        tf.autocorrectionType = .no
         tf.backgroundColor = .white
         return tf
     }()
@@ -66,6 +69,13 @@ class RegistrationViewController: UIViewController {
         super.viewDidLoad()
         setupGradientLayer()
         setupViews()
+        setupObservers()
+        setupGesture()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
     }
     
     func setupViews() {
@@ -87,6 +97,36 @@ class RegistrationViewController: UIViewController {
         }
         
         registerButton.setCornerRadius(value: 25)
+    }
+    
+    fileprivate func setupObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc fileprivate func handleKeyboardHide(_ notification: Notification) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
+            self.view.transform = .identity
+        }, completion: nil)
+    }
+    
+    fileprivate func setupGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleViewTap))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc fileprivate func handleViewTap() {
+        view.endEditing(true)
+    }
+    
+    @objc fileprivate func handleKeyboardShow(_ notification: Notification) {
+        guard let value = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        let keyboardFrame = value.cgRectValue
+        let bottomSpace = view.frame.height - stackView.frame.origin.y - stackView.frame.height
+        // bottom space is space between the bottom of the stackview and the superview
+        let padding: CGFloat = 16
+        let difference = keyboardFrame.height - bottomSpace + padding
+        self.view.transform = CGAffineTransform(translationX: 0, y: -difference)
     }
     
     fileprivate func setupGradientLayer() {
